2769e2054d86d76d0bd0fbf2f58b7cd564491fc5
Alex Valencia <jorgealexandervalencia@gmail.com>
2024-03-01 17:18:45 -0500

    upload spotifymusic08 proyect in react native

---
+++ src/constants.ts
@@ -0,0 +1,50 @@
+import { Track } from "react-native-track-player";
+
+export const playListData: Track[] = [
+    {
+      id: 1,
+      title: 'Look Both Ways',
+      artist: 'Nathan Moore',
+      album: 'Look Both Ways',
+      artwork:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Ff5hqe3E8Uks%2Fhqdefault.jpg&f=1&nofb=1&ipt=5040ea1b10ae2a55e99069ba21f74473dce47654b801c1f2db7f0f5b23afd606&ipo=images',
+      url: require('./assets/audio/one.mp3'),
+    },
+    {
+      id: 2,
+      title: 'Skylines',
+      artist: 'Anno Domini Beats',
+      album: 'Skylines',
+      artwork:
+        'https://i.ytimg.com/vi/Ze5V_RNEv8o/maxresdefault.jpg',
+      url: require('./assets/audio/two.mp3'),
+    },
+    {
+      id: 3,
+      title: 'Eveything Has a Beginning',
+      artist: 'Joel Cummins',
+      album: 'Eveything Has a Beginning',
+      artwork:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F6hRIH1TcRj0%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=beafe57d3feac85429b625d59db63c7d8e997dae3636a6cae4219ee16a1f6e64&ipo=images',
+      url: require('./assets/audio/three.mp3'),
+    },
+    {
+      id: 4,
+      title: 'Od Yishama',
+      artist: 'E´s Jammy Jams',
+      album: 'Od Yishama',
+      artwork:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FE3H8AhheodA%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=15e843af8fdf86ff28a967c66a90d21e931132c419eaa0636cf7489a2e56608a&ipo=images',
+      url: require('./assets/audio/four.mp3'),
+    },
+    {
+      id: 5,
+      title: 'Happy Day',
+      artist: 'film music',
+      album: 'Happy Day',
+      artwork:
+        'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.lovethispic.com%2Fuploaded_images%2F284588-Happy-Day.jpg&f=1&nofb=1&ipt=932f8652d73b4c29de73b720a01d0e9a1e3115b308f34674133d1b6039d2ef92&ipo=images',
+      url: 'https://cdn.filmmusic.io/storage/user_upload/filmmusic/music/mp3low/1531773107-3160.mp3',
+    },
+  ];
+  
